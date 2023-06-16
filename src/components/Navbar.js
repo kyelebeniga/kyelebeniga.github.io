69a40be58dc584835ece7e010fd83e84d1cc6a1b
Kyele Beniga <61011686+kyelebeniga@users.noreply.github.com>
2023-06-16 12:28:35 +0800

    Added dynamic scroll to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/Navbar.css';
 import ReorderIcon from '@mui/icons-material/Reorder';
 
 function Navbar() {
   const [expandNav, setExpandNav] = useState(false);
+  
+  // Function that makes navbar smaller when scrolling
+  const navScroll = () => {
+    const nav = document.querySelector('nav');
+    nav.classList.toggle('sticky', window.scrollY > 100);
+  }
+  useEffect(() => {
+    window.addEventListener('scroll', navScroll);
+    return () => {
+      window.addEventListener('scroll, navScroll');
+    }
+  }, []);
 
   return (
     <>
@@ -22,10 +34,10 @@ function Navbar() {
               </button>
             </div>
             <ul>
-              <li>Home</li>
-              <li>About</li>
-              <li>Projects</li>
-              <li>Contact</li>
+              <li><a href="#home">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#projects">Projects</a></li>
+              <li><a href="#contact">Contact</a></li>
             </ul>
           </div>
         </nav>
