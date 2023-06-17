884c16b05d98846f6c6ba0be4c9fc3f87f1a753b
Kyele Beniga <61011686+kyelebeniga@users.noreply.github.com>
2023-06-17 17:01:18 +0800

    Started building landing page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Home from './components/Home';
 import './styles/App.css';
 
 const App = () => {
     return (
         <div className='App'>
             <Navbar />
+            <Home />
         </div>
     );
 };
