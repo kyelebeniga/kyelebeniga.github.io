335890926b25a8a814f6625ffad7253496715349
Kyele Beniga <61011686+kyelebeniga@users.noreply.github.com>
2023-06-17 17:24:07 +0800

    Fixed navbar bug in mobile view

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import '../styles/Home.css'
 
 function Home() {
   return (
     <>
-      <div className='landingPage'></div>
+      <div className='landingPage'>
+        <h1>Hello, I'm <span>Kyele Beniga.</span></h1>
+        <h2>Developer</h2>
+      </div>
     </>
   )
 }
