658f06f0abebf3f570ab263ae26813a7b292d032
Kyele Beniga <61011686+kyelebeniga@users.noreply.github.com>
2023-06-18 16:04:05 +0800

    Started building about section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import About from './components/About';
 import './styles/App.css';
 
 const App = () => {
--- src/components/About.js
+++ src/components/About.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 function About() {
   return (
-    <div className="aboutContainer">
-        
-    </div>
+    <section className="aboutContainer">
+
+    </section>
   )
 }
 
