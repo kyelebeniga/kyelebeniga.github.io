be718e61c688fe262b159e3eb288d6964fa81cb7
Kyele Beniga <61011686+kyelebeniga@users.noreply.github.com>
2023-06-19 09:42:34 +0800

    Completed About section

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,30 +19,40 @@ function Navbar() {
 
   return (
     <>
-        <nav className="navbar" id={expandNav ? "open" : "close"}>
-          <div className="links">
-            <div className="logo">
-              <h1>
-                Kyele.<span>_</span>
-              </h1>
-            </div>
-            <div className="toggleButton">
-              <button onClick={() => {
-                  setExpandNav((prev) => !prev);
-                }}>
-                <ReorderIcon />
-              </button>
-            </div>
-            <ul>
-              <li><a href="#home">Home</a></li>
-              <li><a href="#about">About</a></li>
-              <li><a href="#projects">Projects</a></li>
-              <li><a href="#contact">Contact</a></li>
-            </ul>
+      <nav className="navbar" id={expandNav ? "open" : "close"}>
+        <div className="toggleButton">
+          <button
+            onClick={() => {
+              setExpandNav((prev) => !prev);
+            }}
+          >
+            <ReorderIcon />
+          </button>
+        </div>
+        <div className="links">
+          <div className="logo">
+            <h1>
+              Kyele.<span>_</span>
+            </h1>
           </div>
-        </nav>
+          <ul>
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#projects">Projects</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </>
-  )
+  );
 }
 
 export default Navbar
